9ebe5c742d15e489226d09d113aa45a3537c953e
leolivm <leolivm@outlook.com>
2022-07-17 18:52:33 -0300

    dynamic data

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,14 +5,34 @@ import { RestaurantImage } from './RestaurantImage'
 import { RestaurantTitle } from './RestaurantTitle'
 import { RestaurantDescription } from './RestaurantDescription'
 
-const image = 'https://github.com/leolivm.png'
-const title = 'Farmhouse Kitchen Thai Cuisine'
-const description = 'Thai • Comfort Food • $$ 🎫  • 4 ⭐️ (2913+)'
+const yelpRestaurantInfo = {
+  name: 'Farmhouse Kitchen Thai Cuisine',
+  image: 'https://github.com/leolivm.png',
+  price: '$$',
+  reviews: '1500',
+  rating: 4.5,
+  categories: [
+    {
+      title: 'Thai',
+    },
+    {
+      title: 'Comfort Food',
+    },
+  ],
+}
+
+const formattedCategories = yelpRestaurantInfo.categories
+  .map((cat) => cat.title)
+  .join(' • ')
+
+const description = `${formattedCategories} ${
+  yelpRestaurantInfo.price ? ' • ' + yelpRestaurantInfo.price : ''
+} • 🎫 • ${yelpRestaurantInfo.rating} ⭐️ (${yelpRestaurantInfo.reviews}+)`
 
 export const About: FC = () => (
   <View>
-    <RestaurantImage image={image} />
-    <RestaurantTitle title={title} />
+    <RestaurantImage image={yelpRestaurantInfo.image} />
+    <RestaurantTitle title={yelpRestaurantInfo.name} />
     <RestaurantDescription description={description} />
   </View>
 )
